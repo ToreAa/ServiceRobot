0023ee64f3803af0a10355611fab4a9c97a7cf7c
ToreAa <aarsland.tore@gmail.com>
2020-01-12 19:50:36 +0000

    added test_navigation.sh

---
+++ catkin_ws/src/my_robot/scripts/test_navigation.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Launch turtlebot in gazeboworld
+xterm -e " source /opt/ros/kinetic/setup.bash;
+source /home/workspace/catkin_ws/devel/setup.bash;
+roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find my_robot)/map/tore.world" &
+sleep 15
+# Launch amcl
+xterm -e " source /opt/ros/kinetic/setup.bash;
+source /home/workspace/catkin_ws/devel/setup.bash;
+roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find my_robot)/map/tore.yaml" &
+sleep 5
+# Launch rviz
+xterm -e " source /opt/ros/kinetic/setup.bash;
+source /home/workspace/catkin_ws/devel/setup.bash;
+roslaunch turtlebot_rviz_launchers view_navigation.launch"
+
+
