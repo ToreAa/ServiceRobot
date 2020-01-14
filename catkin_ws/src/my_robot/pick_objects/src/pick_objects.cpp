ca34f40f70bf73a9de6911eb758df1dfa5488763
ToreAa <aarsland.tore@gmail.com>
2020-01-14 13:02:11 +0000

    added pick_objects

---
+++ catkin_ws/src/my_robot/pick_objects/src/pick_objects.cpp
@@ -0,0 +1,62 @@
+#include <ros/ros.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
+
+// Define a client for to send goal requests to the move_base server through a SimpleActionClient
+typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+
+int main(int argc, char** argv){
+  // Initialize the simple_navigation_goals node
+  ros::init(argc, argv, "pick_objects");
+
+  //tell the action client that we want to spin a thread by default
+  MoveBaseClient ac("move_base", true);
+
+  // Wait 5 sec for move_base action server to come up
+  while(!ac.waitForServer(ros::Duration(5.0))){
+    ROS_INFO("Waiting for the move_base action server to come up");
+  }
+
+  move_base_msgs::MoveBaseGoal goal;
+
+  // set up the frame parameters
+  goal.target_pose.header.frame_id = "map";
+  goal.target_pose.header.stamp = ros::Time::now();
+
+  // Define a goal position 1 to reach
+  goal.target_pose.pose.position.x = -9.25;
+  goal.target_pose.pose.position.y = -7.5;
+
+   // Send the goal position and orientation for the robot to reach
+  ROS_INFO("Sending goal #1");
+  ac.sendGoal(goal);
+
+  // Wait an infinite time for the results
+  ac.waitForResult();
+
+  // Check if the robot reached its goal
+  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    ROS_INFO("Hooray, goal #1 reached");
+  else
+    ROS_INFO("The base failed to reach the goal #1 for some reason");
+
+  // Define a goal position 2 to reach
+  goal.target_pose.pose.position.x = 0.75;
+  goal.target_pose.pose.position.y = -6.75;
+
+   // Send the goal position and orientation for the robot to reach
+  ROS_INFO("Sending goal #2");
+  ac.sendGoal(goal);
+
+  // Wait an infinite time for the results
+  ac.waitForResult();
+
+  // Check if the robot reached its goal
+  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    ROS_INFO("Hooray, goal #2 reached");
+  else
+    ROS_INFO("The base failed to reach the goal #2 for some reason");
+
+
+  return 0;
+}
