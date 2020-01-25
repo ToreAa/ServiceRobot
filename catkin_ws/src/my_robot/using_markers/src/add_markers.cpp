2ec774179a5d6c6d45025102bb54f7ddc8f49e5d
ToreAa <aarsland.tore@gmail.com>
2020-01-25 09:03:18 +0000

    added home_service.sh

--- catkin_ws/src/my_robot/using_markers/src/add_markers.cpp
+++ catkin_ws/src/my_robot/using_markers/src/add_markers.cpp
@@ -1,7 +1,7 @@
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
 #include <nav_msgs/Odometry.h>
-#include <geometry_msgs/PoseStamped.h>
+// #include <geometry_msgs/PoseStamped.h>
 
 double posx;
 double posy;
@@ -14,29 +14,30 @@ void odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
 {
   posx = msg->pose.pose.position.x;
   posy = msg->pose.pose.position.y;
-  // ROS_INFO("Position-> x: [%f], y: [%f]", posx , posy);
+  ROS_INFO("Position-> x: [%f], y: [%f]", posx , posy);
 }
 
+/*
 void poseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg)
 {
   goalx1 = msg->pose.position.x;
   goaly1 = msg->pose.position.y;
   ROS_INFO("Goal-> x: [%f], y: [%f]", goalx1 , goaly1);
-}
+}*/
 
 int main( int argc, char** argv )
 {
   ros::init(argc, argv, "add_markers");
   ros::NodeHandle n;
-  ros::Rate r(1);
+  ros::Rate r(5.0);
   ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
   int i = 0; //integer for stepping through state-machine
 
   while (ros::ok())
   {
     visualization_msgs::Marker marker;
-    ros::Subscriber sub_odom = n.subscribe("/odom", 100, odomCallback);
-    ros::Subscriber sub_goal = n.subscribe("/move_base_simple/goal", 100, poseCallback);
+    ros::Subscriber sub = n.subscribe("/odom", 1.0, odomCallback);
+    //ros::Subscriber sub_goal = n.subscribe("/move_base_simple/goal", 100, poseCallback);
     
     // Set the frame ID and timestamp.  See the TF tutorials for information on these.
     marker.header.frame_id = "/map";
@@ -70,14 +71,28 @@ int main( int argc, char** argv )
         i = 1;
       }
       break;
-    case 1: // Wait for 5 seconds
+    case 1: // Wait until object is picked up
+      ros::Duration(7.0).sleep();
+      i = 2;
+      /*
+      if ((abs (posx-goalx1) > 0.1) || (abs (posy-goaly1) > 0.1)) { 
+        i = 2;
+      }*/
+      break;
+    case 2: // Hide target when first goal is left
       marker.action = visualization_msgs::Marker::DELETEALL; 
       marker_pub.publish(marker);
+      i = 3;
+      break;
+    case 3: // Wait until the second goal is almost reached
       if ((abs (posx-goalx2) < 0.1) && (abs (posy-goaly2) < 0.1)) { //Check if goal is reached
-        i = 2;
+        i = 4;
       }
       break;
-    case 2: // Publish the marker at the drop off zone  
+    case 4: // Wait until the robot has stopped
+      ros::Duration(5.0).sleep();
+      i = 5;
+    case 5:
       marker.action = visualization_msgs::Marker::ADD;
       marker.pose.position.x = goalx2;
       marker.pose.position.y = goaly2;
@@ -91,7 +106,7 @@ int main( int argc, char** argv )
       marker.lifetime = ros::Duration();
       marker_pub.publish(marker);
       ros::Duration(3.0).sleep();
-      i = 0; 
+      // i = 0; 
       break;
     } 
 
@@ -105,7 +120,7 @@ int main( int argc, char** argv )
       sleep(1);
     }
     
-    ros::spinOnce();
     r.sleep();
+    ros::spinOnce();
   }
 }
